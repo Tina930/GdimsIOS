c59d4cc472d3899e9da32b6b583a6c2426e3b2ea
baohongyan <657175417@qq.com>
2017-10-10 17:31:01 +0800

    111

--- GdimsIOS/ViewController.swift
+++ GdimsIOS/ViewController.swift
@@ -12,14 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.blue
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
