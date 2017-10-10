d88a36e137b6d321ba5923145f2d7d8208952384
baohongyan <657175417@qq.com>
2017-10-10 15:42:32 +0800

    first commit

---
+++ GdimsIOS/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  GdimsIOS
+//
+//  Created by 包宏燕 on 2017/10/10.
+//  Copyright © 2017年 bhy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    git init
+    git add .
+    git commit -m "first commit"
+    git remote add origin https://github.com/Tina930/GdimsIOS.git
+    git push -u origin master
+
+}
+
